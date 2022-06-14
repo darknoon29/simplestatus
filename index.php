74046c008aae097277af6499c7132e8ff63820b4
DarkNoon <darknoon@darkcity.fr>
2022-06-14 15:37:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$fp = fsockopen("https://passwordstate.arkadin.lan", 443, $errno, $errstr, 30);
+$fp = fsockopen("passwordstate.arkadin.lan", 443, $errno, $errstr, 30);
 //if the socket failed it's offline...
 if (!$fp) {
     echo "Site not available : $errstr ($errno)<br />\n";
