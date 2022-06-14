b115a935cd36d010e5edcbefdba874cd5ecc14ef
DarkNoon <darknoon@darkcity.fr>
2022-06-14 15:39:59 +0200

    Some UI

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+
+    <title>One IS ASP availibility</title>
+</head>
+
+<body>
+    <header>
+        <h1>Hello,</h1>
+        <p>Welcome to Azure Network Availibity tool !</p>
+    </header>
+
+    <main>
 <?php
+
+echo "Check Site availability Passwordstate using VLAN: <br />\n";
 
 $fp = fsockopen("passwordstate.arkadin.lan", 443, $errno, $errstr, 30);
 //if the socket failed it's offline...
@@ -10,3 +30,12 @@ if (!$fp) {
 }
 
 ?>
+
+</main>
+    <footer>
+        <p>OneIS website.</p>
+    </footer>
+
+</body>
+
+</html>
